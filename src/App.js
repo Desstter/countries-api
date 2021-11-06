c73c6fb62d27a98ace47505cf3799ce7ff7318d7
Santiago Lopez <sanhurta2002@gmail.com>
2021-11-06 13:25:35 -0500

    adding countries

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Form, FloatingLabel } from "react-bootstrap";
+function App() {  
+  const fetchCountries = async () => {
+    await fetch("https://restcountries.com/v3.1/all").then(res => {
+      return res.json()
+    }).then(data => {
 
-function App() {
+      data.forEach(country => {
+
+      
+
+      });
+
+    });
+  };
+  fetchCountries();
   return (
-    <h1>Hola Mundo</h1>
+    <Container className="text-center">
+      <h1>Where Are You From?</h1>
+      <Form>
+        <Form.Group>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="What is your name?"
+            className="mb-3"
+          >
+            <Form.Control type="text" placeholder="Your name" />
+          </FloatingLabel>
+          <Form.Select aria-label="Default select example" options='hola?'>
+            <option>Select Your Country</option>
+          </Form.Select>
+        </Form.Group>
+      </Form>
+    </Container>
   );
 }
 
