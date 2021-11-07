40a045799d6afbe843c58c3f2d92300fc3313f10
Santiago Lopez <sanhurta2002@gmail.com>
2021-11-07 00:35:01 -0500

    feat: add validations and persist data to the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,45 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Select from "react-select";
 import { Container, Form, FloatingLabel, Button } from "react-bootstrap";
-import Axios from 'axios';
+import Axios from "axios";
 import { useState } from "react";
 
+function App() {
+  const [name, setName] = useState("");
+  const [selectedValue, setSelectedValue] = useState('');
 
-function App() {  
-  const [name, setName] = useState('');
-  const [country, setCountry] = useState('');
+  const handleChange = e => {
+    setSelectedValue(e.value);
+  }
   const countries = [{}];
   const fetchCountries = async () => {
-    await fetch("https://restcountries.com/v3.1/all").then(res => {
-      return res.json()
-    }).then(data => { 
-      data.forEach(country => {
-        countries.push({value: country.name.common, label: country.name.common})        
+    await fetch("https://restcountries.com/v3.1/all")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((country) => {
+          countries.push({
+            value: country.name.common,
+            label: country.name.common,
+          });
+        });
       });
-    });
   };
 
   const sendData = () => {
-    Axios.post('http://localhost:3001/add', {
+    Axios.post("http://localhost:3001/add", {
       name: name,
-      country: country } 
-    ).then(() => {console.log(name);})
-  }
+      country: selectedValue,
+    }).then(() => {
+      console.log(name);
+      console.log(selectedValue);
+    });
+  };
+
   fetchCountries();
+
+  
   return (
     <Container>
       <h1>Where Are You From?</h1>
@@ -36,13 +50,21 @@ function App() {
             label="What is your name?"
             className="mb-3"
           >
-            <Form.Control type="text" placeholder="Your name" onChange={(event) => {
-              setName(event.target.value)
-            }} required/>
+            <Form.Control
+              type="text"
+              placeholder="Your name"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+              required
+            />
           </FloatingLabel>
-          <Select className='form-select' aria-label="Default select example" options={countries}/>  
+          <Select className="form-select" options={countries}
+          value={countries.find(obj => obj.value === selectedValue)} onChange={handleChange}/>
         </Form.Group>
-        <Button variant="success" size='lg' onClick={sendData}>Success</Button>{' '}
+        <Button variant="success" size="lg" onClick={sendData}>
+          Success
+        </Button>{" "}
       </Form>
     </Container>
   );
