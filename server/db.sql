0800b8025f2222c07373b9ff988740306378773a
Santiago Lopez <sanhurta2002@gmail.com>
2021-11-07 01:15:46 -0500

    finish <3

--- server/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE countrydb;
-
-USE countrydb;
-
-CREATE TABLE clients_registered(
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(16) NOT NULL,
-    country VARCHAR(16) NOT NULL 
-);
